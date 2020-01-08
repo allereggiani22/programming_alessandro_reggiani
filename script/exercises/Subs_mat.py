66136f92bf1c9d2201fe36f99bf919684d5982f4
Alessandro Reggiani <59025313+allereggiani22@users.noreply.github.com>
2020-01-08 16:24:32 +0100

    upload subs_mat.py

---
+++ script/exercises/Subs_mat.py
@@ -0,0 +1,32 @@
+#Compute the substitution score matrix starting from these alignments
+from math import *
+
+seq1="ACAGGTGGACCTCTATATGG"
+seq2="ACTGGTCGACTTCCGGATCG"
+
+def freq_nucl(seq1,se12):
+    freq_sing={}
+    nucl=["A","C","T","G"]
+    for base in nucl:
+        freq_sing[base]=((seq1+seq2).count(base))/float(len(seq1+seq2))
+    return freq_sing
+
+def freq_comb(seq1,seq2):
+    if len(seq1)==len(seq2):
+        nucl=["A","C","T","G"]
+        freq_pair={}
+        fn=freq_nucl(seq1,seq2)
+        for i in nucl:
+            for j in nucl:
+                freq_pair[i+j]=1
+        for n in range(len(seq1)):
+            freq_pair[seq1[n]+seq2[n]]=freq_pair.get(seq1[n]+seq2[n],1)+1
+        for key in freq_pair:
+            freq_pair[key]=log((freq_pair[key]/float(len(seq1)))/(fn[key[0]]*fn[key[1]]))
+        for i in nucl:
+            l=[]
+    else:
+        return "Sequences are different in lenght!"
+
+print freq_comb(seq1,seq2)
+
