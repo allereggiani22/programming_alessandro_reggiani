b9484650d9b47e45fd880fd0e81bd824f0f7d7a2
Alessandro Reggiani <59025313+allereggiani22@users.noreply.github.com>
2020-01-11 11:59:55 +0100

    Added function definitions

--- script/exercises/Blosum50.py
+++ script/exercises/Blosum50.py
@@ -1,30 +1,33 @@
 from re import *
-f= open("./Blosum50.txt","r")
-blosum=[]
-for line in f:
-    line=line.rstrip()
-    line=line.split()
-    blosum.append(line)
-f.close()
-for list in blosum[1:]:
-    for i in range(1,len(list)):
-        if list[i] != "[ARNDCQEGHILKMFPSTWYV]":
-            list[i]=int(list[i])
 
-#print blosum
+def blosum50(file):
+    """This function takes a file as input and generates a dictionary with the blosum matrix"""
+    blosum=[]
+    for line in file:
+        line=line.rstrip()
+        line=line.split()
+        blosum.append(line)
+    file.close()
+    for list in blosum[1:]:
+        for i in range(1,len(list)):
+            if list[i] != "[ARNDCQEGHILKMFPSTWYV]":
+                list[i]=int(list[i])
+    blosum50 = {}
+    for i in range(1, len(blosum)):
+        for j in range(len(blosum[0])):
+            blosum50[blosum[i][0] + blosum[0][j]] = blosum[i][j + 1]
+    return blosum50
 
-blosum50={}
-for i in range(1,len(blosum)):
-    for j in range(len(blosum[0])):
-        blosum50[blosum[i][0]+blosum[0][j]]=blosum[i][j+1]
 
-#print blosum50["KK"]
+
+#print blosum50(open("./Blosum50.txt","r"))
         
 def score_prot(seq1,seq2):
+    b=blosum50(open("./Blosum50.txt","r"))
     if len(seq1)==len(seq2):
         score=0
         for i in range(len(seq1)):
-            score = score + blosum50[seq1[i]+seq2[i]]
+            score = score + b[seq1[i]+seq2[i]]
         return score
     else:
         return "Sequences are different in lenght!"
